338a4389f69a0de4d28707bc441528597438ca49
byaccident <mckruse@gmail.com>
2015-08-22 17:31:49 -0400

    Rprogramming Assignment 2
    caches an inverted matrix for later retrieval to reduce re-computation
    of inverted matrices.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions cache the inverse for a matrix 'x', and store for later retrieval to reduce repeat calculations
 
-## Write a short comment describing this function
+## The following function makes a cache of the argument 'x', a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mInv<-NULL
+  set<-function(y){
+    x<<-y
+    mInv<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) mInv<<- solve
+  getmatrix<-function() mInv
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+## The following function solves for the inverse of the matrix supplied in cacheMatrix and retrieves this value 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+
+cacheSolve <- function(x=matrix(), ...) {
+  mInv<-x$getmatrix()
+  if(!is.null(mInv)){
+    message("getting cached data")
+    return(mInv)
+  }
+  matrix<-x$get()
+  mInv<-solve(matrix, ...)
+  x$setmatrix(mInv)
+  mInv
 }
